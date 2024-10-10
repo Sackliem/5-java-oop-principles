9a2f9b7c0d43d334a4a79960d61e5ab924219bdb
Maksim Lyubimov <sackliem@mail.ru>
2024-10-10 15:22:23 +0500

    init

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,10 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("test1", LogLevel.DEBUG);
+        logger.setLevel(LogLevel.DEBUG);
+        logger.setHandler(new ConsoleHandler());
+        logger.debug("test2");
+        logger.error("test3");
     }
 }
